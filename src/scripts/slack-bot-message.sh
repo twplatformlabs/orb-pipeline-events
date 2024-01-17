44f3dd8ebbfe459e7e0c9ca31deaa2fee339a0c1
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-01-17 16:18:55 -0600

    [nc] adding orb release slack message to orb pipeline
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/slack-bot-message.sh
+++ src/scripts/slack-bot-message.sh
@@ -5,19 +5,26 @@ echo "CHANNEL=$CHANNEL"
 echo "MESSAGE=$MESSAGE"
 echo "CUSTOM_MESSAGE=$CUSTOM_MESSAGE"
 echo "INCLUDE_LINK=$INCLUDE_LINK"
-echo "CIRCLE_BUILD_URL=$CIRCLE_BUILD_URL" 
+echo "CIRCLE_BUILD_URL=$CIRCLE_BUILD_URL"
+echo "INCLUDE_TAG=$INCLUDE_TAG"
+
+if [[ $INCLUDE_TAG == 0]]; then
+  msg=$MESSAGE
+else
+  msg="$MESSAGE $CIRCLE_TAG"
+fi
 
 if [[ "$CUSTOM_MESSAGE" != "" ]]; then
   json=$CUSTOM_MESSAGE
 else
   if [[ $INCLUDE_LINK == 0 ]]; then
       json=$(cat <<EOF
-{"channel": "$CHANNEL","blocks":[{"type":"section","text":{"type":"mrkdwn","text":"$MESSAGE"}}]}
+{"channel": "$CHANNEL","blocks":[{"type":"section","text":{"type":"mrkdwn","text":"$msg"}}]}
 EOF
 )
   else
       json=$(cat <<EOF
-{"channel": "$CHANNEL","blocks": [{"type": "section","text": {"type": "mrkdwn","text": "$MESSAGE"}},{"type": "divider"},{"type": "actions","elements": [{"type": "button","text": {"type": "plain_text","text": "go to pipeline","emoji": true},"url": "$CIRCLE_BUILD_URL"}]}]}
+{"channel": "$CHANNEL","blocks": [{"type": "section","text": {"type": "mrkdwn","text": "$msg"}},{"type": "divider"},{"type": "actions","elements": [{"type": "button","text": {"type": "plain_text","text": "go to pipeline","emoji": true},"url": "$CIRCLE_BUILD_URL"}]}]}
 EOF
 )
   fi
