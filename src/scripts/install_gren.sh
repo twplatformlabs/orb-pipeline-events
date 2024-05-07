39d10e413e5e6750cd32b73165f1be35da259759
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-05-07 14:24:54 -0500

    [nc] adjust install
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_gren.sh
+++ src/scripts/install_gren.sh
@@ -4,9 +4,9 @@ set -eo pipefail
 echo "gren install depends on nodejs and npm."
 
 if [[ "$GREN_VERSION" == "latest" ]]; then
-    npm install -g gren
+    npm install -g github-release-notes
 else
-    npm install -g gren@"$GREN_VERSION"
+    npm install -g github-release-notes@"$GREN_VERSION"
 fi
 
 gren -v
