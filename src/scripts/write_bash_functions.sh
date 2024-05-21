f55885fb585548507564e9fd53c13083a9f62fea
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-05-21 17:57:33 -0500

    [nc] add write1passwordField function
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/write_bash_functions.sh
+++ src/scripts/write_bash_functions.sh
@@ -21,4 +21,29 @@ awsAssumeRole () {
     export AWS_SESSION_TOKEN=$(cat credentials | jq -r ".Credentials.SessionToken")
 }
 
+# write1passwordField ()  ================================================================================
+#
+# update field value in 1password vault item, will creat item if it does not exists
+# Expects parameters
+# $1 = vault name
+# $2 = item name
+# $3 = field name
+# $4 = new value
+# Expects 1password service account credential to be present
+
+write1passwordField () {
+    set +e
+    op item list --vault $1 | grep $2 >/dev/null
+    exists=$?
+    if [[ "$exists" == 1 ]]; then
+        echo "$2 not found, creating..."
+        echo "updating $3..."
+        op item create --title="$2" --category 'API Credential' --vault "$1" "$3"="$4" >/dev/null
+    else
+        echo "updating $3..."
+        op item edit "$2" "$3=$4" --vault $1 >/dev/null
+    fi
+    set -e
+}
+
 EOF
