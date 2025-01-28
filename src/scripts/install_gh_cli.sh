6385228c24e0df4b381cda7e715638929176e9e1
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-01-28 17:23:48 -0600

    [nc] install support for machine execution environments only
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_gh_cli.sh
+++ src/scripts/install_gh_cli.sh
@@ -1,27 +1,10 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-install_alpine() {
-  if [[ "$GH_CLI_VERSION" == "latest" ]]; then
-    sudo apk add --no-cache github-cli
-  else
-    sudo apk add --no-cache github-cli=="$GH_CLI_VERSION"
-  fi
-}
-
-install_ubuntu() {
-  if [[ "$GH_CLI_VERSION" == "latest" ]]; then
-    sudo apt-get install --no-install-recommends -y gh
-  else
-    sudo apt-get install --no-install-recommends -y gh="$GH_CLI_VERSION"
-  fi
-
-}
-
-if [[ "$OS" == "Alpine" ]]; then
-    install_alpine
-elif [[ "$OS" == "Ubuntu" ]]; then
-    install_ubuntu
+if [[ "$GH_CLI_VERSION" == "latest" ]]; then
+  sudo apt-get install --no-install-recommends -y gh
+else
+  sudo apt-get install --no-install-recommends -y gh="$GH_CLI_VERSION"
 fi
 
 gh --version
