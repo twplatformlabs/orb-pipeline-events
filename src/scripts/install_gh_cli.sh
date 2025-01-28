c41f5b82549cd5d9281638c087a6fd8649d8f7b9
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-01-28 17:28:28 -0600

    [nc] add gh cli install config
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/install_gh_cli.sh
+++ src/scripts/install_gh_cli.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+# add repository info for install of github cli
+mkdir -p -m 755 /etc/apt/keyrings
+curl -s https://cli.github.com/packages/githubcli-archive-keyring.gpg | tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
+chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
 if [[ "$GH_CLI_VERSION" == "latest" ]]; then
   sudo apt-get install --no-install-recommends -y gh
 else
