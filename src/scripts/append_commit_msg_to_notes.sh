ee6ceeecd07b38f52ce42c9f373dd8624bad1112
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-01-31 13:47:57 -0600

    [nc] lint corrections
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/append_commit_msg_to_notes.sh
+++ src/scripts/append_commit_msg_to_notes.sh
@@ -3,9 +3,9 @@ set -eo pipefail
 
 echo "append commit messages since last release to notes"
 
-messages=$(git log --oneline $(git describe --tags --abbrev=0 @^)..@)
+messages=$(git log --oneline "$(git describe --tags --abbrev=0 @^)"..@)
 
-cat <<EOF >> $OUTFILE
+cat <<EOF >> "$OUTFILE"
 <details>
 <summary>Commits included</summary>
 $messages
