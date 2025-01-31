58ff19c7e616587bb8af3b7f103c4bbc793cfb91
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-01-31 15:47:12 -0600

    [nc] debug artifact uploads and commit message notes in gh release
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/append_commit_msg_to_notes.sh
+++ src/scripts/append_commit_msg_to_notes.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 echo "append commit messages since last release to notes"
 
 messages=$(git log --oneline "$(git describe --tags --abbrev=0 @^)"..@)
-messages=${messages//$'\n'/$'  \n'}
+messages=${messages//$'\n'/$'<br>\n'}
 echo "commit messages to be appended: $messages"
 
 cat <<EOF >> "$OUTFILE"
