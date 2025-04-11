a812549d497f2bc3456ccd154cb50ea5f1e1e4f9
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-04-11 08:37:28 -0500

    [nc] add committer info to auto-tag function
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- src/scripts/tag_monthly_recurring.sh
+++ src/scripts/tag_monthly_recurring.sh
@@ -3,6 +3,13 @@
 set -eo pipefail
 source bash-functions.sh
 
+committer_email=$1
+committer_nanme=$2
+
+# set committer
+  git config --global user.email "$1"
+  git config --global user.name "$2"
+
 # generate monthly-recurring tag value
 year=$(date +%Y)
 month=$(date +%m)
